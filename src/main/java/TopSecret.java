e9412bbb4b11fcf3d77eccf50e0160ada9c7bf3c
Chloe Wang <chloewang@Chloes-MacBook-Air.local>
2026-02-05 14:08:11 -0500

    test folder of random files

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -1,5 +1,41 @@
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Commmand Line Utility
  */
 public class TopSecret {
+    public void accessFiles(String[] args){         //this method attempts to access files from the data folder
+
+        List<String> files = new ArrayList<>();      //create list of files.
+        Path dataPath = Paths.get("data");      //path to the files; I created a new folder under TopSecret with random files a.txt and b.txt
+
+        if (args.length == 0){ //if arguments = 0
+            int counter = 0;
+            // using try catch for stuff involving file systems.
+            try (DirectoryStream<Path> stream = Files.newDirectoryStream(dataPath)) { //opens the system for files
+                for (Path aFile : stream) {  //for every file in the stream
+                    files.add(aFile.getFileName().toString()); //converts the filename to string and adds it into the prepared arraylist
+                    if (counter < 10) {
+                        System.out.println("0" + counter + " " + aFile.getFileName());
+                        counter++;
+                    }
+                    else {
+                        System.out.println(counter + " " + aFile.getFileName());
+                        counter++;
+                    }
+
+                }
+            } catch (IOException e) { //stream is automatically closed by now.
+                System.out.println("Some error in accessing files.");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
 }
